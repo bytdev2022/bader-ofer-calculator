bbe3c1a39182b71c368b34e89e4652be51f85151
Yitzhak Amsalem <yitzhak@gmail.com>
2022-10-26 18:33:46 +0300

    surplus vote agreement

--- src/App.js
+++ src/App.js
@@ -3,6 +3,71 @@ import "./App.css";
 
 const ALL_MANDATES = 120;
 const BLOCKAGE_THRESHOLD_RATE = 3.25;
+const groups = [
+    {
+        "names": ['הליכוד', 'הציונות הדתית'],
+        "votes": 0,
+        "mandates": 0,
+        "moreMandates": 0,
+        "next_mandate_votes_per_mandate": 0
+    },
+    {
+        "names": ['יש עתיד', 'המחנה הממלכתי'],
+        "votes": 0,
+        "mandates": 0,
+        "moreMandates": 0,
+        "next_mandate_votes_per_mandate": 0
+    },
+    {
+        "names": ['יהדות התורה', 'ש"ס'],
+        "votes": 0,
+        "mandates": 0,
+        "moreMandates": 0,
+        "next_mandate_votes_per_mandate": 0
+    },
+    {
+        "names": ['העבודה', 'מרץ'],
+        "votes": 0,
+        "mandates": 0,
+        "moreMandates": 0,
+        "next_mandate_votes_per_mandate": 0
+    },
+    {
+        "names": ['ישראל ביתנו'],
+        "votes": 0,
+        "mandates": 0,
+        "moreMandates": 0,
+        "next_mandate_votes_per_mandate": 0
+    },
+    {
+        "names": ['חד"ש תע"ל'],
+        "votes": 0,
+        "mandates": 0,
+        "moreMandates": 0,
+        "next_mandate_votes_per_mandate": 0
+    },
+    {
+        "names": ['רע"מ'],
+        "votes": 0,
+        "mandates": 0,
+        "moreMandates": 0,
+        "next_mandate_votes_per_mandate": 0
+    },
+    {
+        "names": ['בל"ד'],
+        "votes": 0,
+        "mandates": 0,
+        "moreMandates": 0,
+        "next_mandate_votes_per_mandate": 0
+    },
+    {
+        "names": ['הבית היהודי'],
+        "votes": 0,
+        "mandates": 0,
+        "moreMandates": 0,
+        "next_mandate_votes_per_mandate": 0
+    }
+];
 
 class App extends Component {
     state = {
@@ -126,41 +191,54 @@ class App extends Component {
             },
         ],
         results: false,
-        isHovering: false
     }
     calculate = () => {
         this.state.partiesResults.forEach((party) => {
             party.mandates = 0;
             party.next_mandate_votes_per_mandate = 0;
             party.moreMandates = 0;
-
         })
-
         let allVotes = 0;
         for (let i = 0; i < this.state.partiesResults.length; i++) {
             allVotes += this.state.partiesResults[i].votes;
         }
         let blockageThreshold = (allVotes / 100) * BLOCKAGE_THRESHOLD_RATE
         let partiesOverBlockageThreshold = this.state.partiesResults.filter(party => party.votes >= blockageThreshold);
-
         let allCountedVotes = 0;
         for (let i = 0; i < partiesOverBlockageThreshold.length; i++) {
             allCountedVotes += partiesOverBlockageThreshold[i].votes;
         }
-
         let MandateSurveyor = allCountedVotes / ALL_MANDATES;
         partiesOverBlockageThreshold.forEach(party => {
-            party.mandates = parseInt((party.votes / MandateSurveyor).toString());
+            party.mandates = Math.floor(party.votes / MandateSurveyor);
             party.next_mandate_votes_per_mandate = party.votes / (party.mandates + 1);
-
         })
 
-        while (this.remainedMandates(partiesOverBlockageThreshold) > 0) {
-            let partyToAddMandate = partiesOverBlockageThreshold[this.max(partiesOverBlockageThreshold)];
-            partyToAddMandate.moreMandates += 1
-            partyToAddMandate.next_mandate_votes_per_mandate = partyToAddMandate.votes / (
-                partyToAddMandate.mandates + partyToAddMandate.moreMandates + 1)
-            partiesOverBlockageThreshold[partyToAddMandate] = partyToAddMandate
+        this.updateGroupsFromParties(partiesOverBlockageThreshold);
+        while (this.remainedMandates(groups) > 0) {
+            let groupIndexToAddMandate = this.max(groups);
+            groups[groupIndexToAddMandate].moreMandates += 1;
+            groups[groupIndexToAddMandate].next_mandate_votes_per_mandate = groups[groupIndexToAddMandate].votes / (
+                groups[groupIndexToAddMandate].mandates + groups[groupIndexToAddMandate].moreMandates + 1)
+        }
+
+        for (let i = 0; i < groups.length; i++) {
+            let groupMandates = groups[i].mandates + groups[i].moreMandates;
+            let votesPerMandate = groups[i].votes / groupMandates;
+            for (let j = 0; j < groups[i].names.length; j++) {
+                let partyIndex = partiesOverBlockageThreshold.findIndex(party => party.name === groups[i].names[j])
+                if (partyIndex >= 0){
+                    partiesOverBlockageThreshold[partyIndex].mandates = Math.floor(partiesOverBlockageThreshold[partyIndex].votes / votesPerMandate)
+                    partiesOverBlockageThreshold[partyIndex].next_mandate_votes_per_mandate = partiesOverBlockageThreshold[partyIndex].votes / (partiesOverBlockageThreshold[partyIndex]["mandates"] + 1);
+                }
+            }
+            let groupParties = partiesOverBlockageThreshold.filter((party) => groups[i]["names"].includes(party["name"]))
+            let groupPartiesMandates = this.sumOfPropertyInObjectsArray(groupParties, ["mandates"])
+            if (groupPartiesMandates < groupMandates) {
+                let partyNameToAddMandate = groupParties[this.max(groupParties)].name;
+                let partyIndex = partiesOverBlockageThreshold.findIndex(party => party.name === partyNameToAddMandate)
+                partiesOverBlockageThreshold[partyIndex]["moreMandates"] += 1
+            }
         }
         this.state.partiesResults.sort(function (a, b) {
             return (b.mandates + b.moreMandates) - (a.mandates + a.moreMandates)
@@ -168,28 +246,46 @@ class App extends Component {
         this.setState({
             results: true
         })
-
     }
 
-    max = (partiesOverBlockageThreshold) => {
+    updateGroupsFromParties = (partiesOverBlockageThreshold)=>{
+        for (let i =0; i < groups.length; i++){
+            let group = partiesOverBlockageThreshold.filter((party)=> groups[i]["names"].includes(party["name"]))
+            groups[i]["votes"] = this.sumOfPropertyInObjectsArray(group, ["votes"])
+            groups[i]["mandates"] = this.sumOfPropertyInObjectsArray(group, ["mandates"])
+            groups[i]["next_mandate_votes_per_mandate"] = groups[i].votes / (groups[i].mandates + 1)
+        }
+    }
+
+    sumOfPropertyInObjectsArray = (arrayOfObjects, props) => {
+        let sum = 0;
+        arrayOfObjects.forEach(obj => {
+            props.forEach(prop => {
+                sum += obj[prop]
+            })
+        })
+        return sum
+    }
+
+    max = (groups) => {
         let index = 0;
         let max = 0;
-        for (let i = 0; i < partiesOverBlockageThreshold.length; i++) {
-            if (max < partiesOverBlockageThreshold[i].next_mandate_votes_per_mandate) {
-                max = partiesOverBlockageThreshold[i].next_mandate_votes_per_mandate;
+        for (let i = 0; i < groups.length; i++) {
+            if (max < groups[i].next_mandate_votes_per_mandate) {
+                max = groups[i].next_mandate_votes_per_mandate;
                 index = i;
             }
         }
         return index;
     }
-    remainedMandates = (partiesOverBlockageThreshold) => {
+
+    remainedMandates = (groups) => {
         let sum = 0;
-        for (let i = 0; i < partiesOverBlockageThreshold.length; i++) {
-            sum += (partiesOverBlockageThreshold[i].mandates + partiesOverBlockageThreshold[i].moreMandates);
+        for (let i = 0; i < groups.length; i++) {
+            sum += (groups[i].mandates + groups[i].moreMandates);
         }
         return (ALL_MANDATES - sum);
     }
-
 
     onClear = () => {
         this.state.partiesResults.forEach((party) => {
@@ -252,7 +348,10 @@ class App extends Component {
                             this.state.partiesResults.map((item, i) => {
                                 return (
                                     <tr key={i}>
-                                        <td style={{fontWeight: "bold", backgroundColor: (item.moreMandates > 0) ? "SpringGreen" : null}}>
+                                        <td style={{
+                                            fontWeight: "bold",
+                                            backgroundColor: (item.moreMandates > 0) ? "SpringGreen" : null
+                                        }}>
                                             {item.name}
                                         </td>
                                         <td>
@@ -275,7 +374,10 @@ class App extends Component {
                                         <td>
                                             {item.mandates}
                                         </td>
-                                        <td style={{fontWeight: "bold", backgroundColor: (item.moreMandates > 0) ? "SpringGreen" : null}}>
+                                        <td style={{
+                                            fontWeight: "bold",
+                                            backgroundColor: (item.moreMandates > 0) ? "SpringGreen" : null
+                                        }}>
                                             {item.moreMandates + item.mandates}
                                         </td>
                                     </tr>
